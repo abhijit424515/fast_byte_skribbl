1294e00f79723d9fe91d7dad7749ba2b3c8a4c25
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2022-11-26 02:05:30 +0530

    try-2

--- client/src/components/SoundMap.jsx
+++
@@ -1,7 +0,0 @@
-const soundMap = {
-	buttonClick: "./assets/buttonClick.wav",
-	messageSent: "./assets/messageSent.mp3",
-	correctAnswer: "./assets/correctAnswer.mp3",
-};
-
-export default soundMap;
